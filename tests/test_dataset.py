9c9f447480810b0ecedf34a89f552a9304a63ed0
marcosgabrielsr <marcosdevse@gmail.com>
2026-08-01 16:27:30 -0400

    feat: Testing new methods (get_components)

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,7 +1,9 @@
 from src.core.dataset import Dataset
 
 ds = Dataset.from_csv("data/raw/dadosIniciais.csv")
-print(len(ds))                     # ~48384
-print(ds.x[0], ds.y[0])            # dois números complexos
+print(len(ds))                            # ~48384
+print(ds.x[0], ds.y[0])                   # dois números complexos
+ds_comp = ds.get_components()             # analisa as components
+print(ds_comp.keys())
 tr, va, te = ds.split()
-print(len(tr), len(va), len(te))   # ~29030 / 9677 / 9677, somando o total
+print(len(tr), len(va), len(te))          # ~29030 / 9677 / 9677, somando o total
