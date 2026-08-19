a0608421fbc25174ddd3d71095ae9d3d2e80b91e
marcosgabrielsr <marcosdevse@gmail.com>
2026-08-19 16:41:15 -0400

    feat: Modularizating the grid search structure

--- src/search/lstm_grid_search.py
+++ src/search/lstm_grid_search.py
@@ -2,80 +2,21 @@ import json
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.optim as optim
 from pathlib import Path
 from src.models.lstm import PaLSTM
+from src.models.trainer_lstm import TrainerLSTM
 from src.core.dataset import Dataset
-from src.metrics.metrics import RMSE,EVM
 from src.core.sliding_window import SlidingWindowDataset
 from torch.utils.data import DataLoader
 from itertools import product
 
-class TrainerLSTM():
-    def __init__(self,model, criterion, lr, scaler, optimizer=None, device=torch.device('cpu')):
-        self.model = model
-        self.criterion = criterion
-        self.optimizer = optim.Adam(model.parameters(), lr=lr) if optimizer is None else optimizer
-        self.device = device
-        self.scaler = scaler
-
-    def fit(self,train_loader):
-        """Treina o modelo LSTM por uma época."""
-        self.model.train()
-        metric_rmse, metric_evm = RMSE(), EVM()
-        y_pred, y_true = [], []
-        total_loss, total = 0.0, 0
-        
-        for inputs, targets in train_loader:
-            inputs, targets = inputs.to(self.device), targets.to(self.device)
-
-            self.optimizer.zero_grad()
-            outputs = self.model(inputs)
-            loss = self.criterion(outputs, targets)
-            loss.backward()
-            self.optimizer.step()
-
-            total_loss += loss.item() * inputs.size(0)
-            y_pred.append(outputs.detach().cpu())
-            y_true.append(targets.cpu())
-            total += outputs.size(0)
-
-        y_pred, y_true = np.concatenate(y_pred,axis=0), np.concatenate(y_true,axis=0)
-        y_pred, y_true = self.scaler.inverse_transform(y_pred), self.scaler.inverse_transform(y_true)
-        avg_loss = total_loss / total
-        rmse = metric_rmse.compute(y_true, y_pred)
-        evm = metric_evm.compute(y_true, y_pred)
-        return avg_loss, rmse, evm
-
-    def evaluate(self,val_loader):
-        """Avalia o modelo LSTM no conjunto de validação."""
-        self.model.eval()
-        metric_rmse, metric_evm = RMSE(), EVM()
-        y_pred, y_true = [], []
-        total_loss, total = 0.0, 0
-
-        with torch.no_grad():
-            for inputs, targets in val_loader:
-                inputs, targets = inputs.to(self.device), targets.to(self.device)
-
-                outputs = self.model(inputs)
-                loss = self.criterion(outputs, targets)
-
-                total_loss += loss.item() * inputs.size(0)
-                y_pred.append(outputs.detach().cpu())
-                y_true.append(targets.cpu())
-                total += outputs.size(0)
-
-        y_pred, y_true = np.concatenate(y_pred,axis=0), np.concatenate(y_true,axis=0)
-        y_pred, y_true = self.scaler.inverse_transform(y_pred), self.scaler.inverse_transform(y_true)
-        avg_loss = total_loss / total
-        rmse = metric_rmse.compute(y_true, y_pred)
-        evm = metric_evm.compute(y_true, y_pred)
-        return avg_loss, rmse, evm
-
-class ExperimentMetricsAnalyser():
+class GridSearchMetricsAnalyser():
     # Armazena as métricas, melhores modelos e seus respectivos hiperparâmetros e histórico (loss de treino e validação) para a avaliação final
-    def __init__(self):
+    def __init__(self, save_path):
+        # Diretório para salvar os resultados
+        self.save_path = Path(save_path)
+        (self.save_path/"models").mkdir(parents=True, exist_ok=True)
+
         # Estrutura de chaves para salvar os hiperparâmetros
         self.RESULTS_PARAM_KEYS = RESULTS_PARAM_KEYS = ("ws", "hs", "nl", "lr", "dr", "bs", "ep")
         self.HISTORY_VALUES_KEYS = ("train_avg_loss", "train_rmse", "train_evm", "val_avg_loss", "val_rmse", "val_evm")
@@ -84,17 +25,14 @@ class ExperimentMetricsAnalyser():
         self.results = {
             'rmse': {
                 'value': float('inf'),
-                'state_model': None,
                 'hyperparameters': None,
             },
             'evm': {
                 'value': float('inf'),
-                'state_model': None,
                 'hyperparameters': None,
             },
             'avg_loss': {
                 'value': float('inf'),
-                'state_model': None,
                 'hyperparameters': None,
             }
         }
@@ -108,16 +46,19 @@ class ExperimentMetricsAnalyser():
 
     def update_results(self, rmse, evm, avg_loss, model, params):
         for k, metric in zip(self.results.keys(),(rmse, evm, avg_loss)):
-            if self.comp(self.results[k]['value'],metric):
+            if self.comp(metric, self.results[k]['value']):
                 self.results[k]['value'] = metric
-                self.results[k]['state_model'] = model.state_dict()
                 self.results[k]['hyperparameters'] = self.get_params_dict(params)
+                torch.save(model.state_dict(), self.save_path/"models"/f"best_{k}_lstm.pkl")
 
     def update_history(self, key_hyperparams, values):
         # Atualiza o histórico das métricas de treino para cada modelo na ordem (train_avg_loss, train_rmse, train_evm, val_avg_loss, val_rmse, val_evm)
-        if key_hyperparams[:-1] not in self.history:
+        key = str(key_hyperparams[:-1])
+        n_epochs = key_hyperparams[-1]
+
+        if (key not in self.history) or (n_epochs not in self.history[key]):
             history_element = {
-                f"{key_hyperparams[-1]}": {
+                n_epochs: {
                     "train_avg_loss": [values[0]],
                     "train_rmse": [values[1]],
                     "train_evm": [values[2]],
@@ -126,49 +67,38 @@ class ExperimentMetricsAnalyser():
                     "val_evm": [values[5]]
                 }
             }
-            self.history[f"{key_hyperparams[:-1]}"] = history_element
+            self.history[key] = history_element
         else:
-            key = f"{key_hyperparams[:-1]}"
-            n_epochs = key_hyperparams[-1]
             for metrics,value in zip(self.HISTORY_VALUES_KEYS,values):
                 self.history[key][n_epochs][metrics].append(value)
 
-    def save(self, save_path):
+    def save(self):
         # Salva os resultados dos melhores modelos e o histórico das métricas de treino e validação
-        with open(f"{save_path}/best_results.json", "w") as f:
+        with open(f"{self.save_path}/best_results.json", "w") as f:
             json.dump(self.results, f, indent=2)
 
-        with open(f"{save_path}/history.json", "w") as f:
+        with open(f"{self.save_path}/history.json", "w") as f:
             json.dump(self.history, f, indent=2)
 
 class GridSearchLSTM():
-    def __init__(self, train, val, test, param_grid):
+    def __init__(self, train, val, test, param_grid, device):
         self.train = train
         self.val = val
         self.test = test
-        self.param_grid = param_grid
-        self.train_norm, self.val_norm, self.test_norm, self.scaler_y = self.normalize_datasets(train, val, test)
-        self.scaler = None
-
-    def get_param_combinations(self):
-        return (
-            list(
-                product(
-                    self.param_grid["windows_size"],
-                    self.param_grid["hidden_size"],
-                    self.param_grid["num_layers"],
-                    self.param_grid["learning_rate"],
-                    self.param_grid["dropout"],
-                    self.param_grid["batch_size"],
-                    self.param_grid["epoch"]
-                )
-            )
-        )
+        self.param_grid_combinations = param_grid
+        self.train_norm, self.scaler_x, self.scaler_y = train.normalize(train)
+        self.val_norm = val.normalize(val, self.scaler_x, self.scaler_y)
+        self.test_norm = test.normalize(test, self.scaler_x, self.scaler_y)
+        self.device = device
+        list(product(self.param_grid["windows_size"],self.param_grid["hidden_size"],self.param_grid["num_layers"],self.param_grid["learning_rate"],self.param_grid["dropout"],self.param_grid["batch_size"],self.param_grid["epoch"]))
 
     def run(self, save_path):
         # Executa o grid search sobre os hiperparâmetros e salva os resultados dos melhores modelos em arquivos
         params_combination = self.get_param_combinations()
         n_combinations = len(params_combination)
+
+        # Configurando classe para análise dos resultados
+        result_analyser = GridSearchMetricsAnalyser(save_path)
 
         for i, (ws, hs, nl, lr, dr, bs, n_epochs) in enumerate(params_combination):
             # Cria o modelo LSTM com os hiperparâmetros atuais
@@ -184,18 +114,15 @@ class GridSearchLSTM():
             val_loader = DataLoader(slw_val, batch_size=bs, shuffle=False)
 
             # Configurando classe para treinar lstm
-            trainer = TrainerLSTM(model,nn.MSELoss,lr,self.scaler_y)
-
-            # Configurando classe para análise dos resultados
-            result_analyser = ExperimentMetricsAnalyser()
+            trainer = TrainerLSTM(model,nn.MSELoss(),lr,self.scaler_y,device=self.device)
 
             print(f"Combination ({i+1}/{n_combinations}): WS = {ws}, HS = {hs}, NL = {nl}, LR = {lr}, DR = {dr}, BS = {bs}, EPOCHS = {n_epochs}\n")
-            for epoch in range(n_epochs):
+            for epoch in range(0, n_epochs):
                 hyperparams = (ws, hs, nl, lr, dr, bs, n_epochs)
                 train_avg_loss, train_rmse, train_evm = trainer.fit(train_loader)
                 val_avg_loss, val_rmse, val_evm = trainer.evaluate(val_loader)
 
-                if (epoch+1)%(n_epochs//10) == 0:
+                if (epoch+1)%(max(1, n_epochs//10)) == 0:
                     print(f"\t({epoch+1}/{n_epochs}):")
                     print(f"\t\ttrain | rmse: {train_rmse:.6f}, evm: {train_evm:.6f}, avg_loss: {train_avg_loss:.6f}")
                     print(f"\t\tval   | rmse: {val_rmse:.6f}, evm: {val_evm:.6f}, avg_loss: {val_avg_loss:.6f}\n")
