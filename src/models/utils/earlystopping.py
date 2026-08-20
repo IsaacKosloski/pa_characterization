9963277029c82815e9306e0b6b1cf0cf0c7579a1
marcosgabrielsr <marcosdevse@gmail.com>
2026-08-19 21:29:27 -0400

    feat: Add best_model attribute

--- src/models/utils/earlystopping.py
+++ src/models/utils/earlystopping.py
@@ -5,12 +5,14 @@ class EarlyStopping:
         self.delta = delta
         self.verbose = verbose
         self.best_metric = None
+        self.best_model = None
         self.no_improvement_count = 0
         self.stop_training = False
 
-    def check_early_stop(self, val_metric):
+    def check_early_stop(self, val_metric, model):
         if self.best_metric is None or val_metric < self.best_metric - self.delta:
             self.best_metric = val_metric
+            self.best_model = model
             self.no_improvement_count = 0
         else:
             self.no_improvement_count += 1
