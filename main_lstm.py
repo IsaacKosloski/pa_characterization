19b63101508114e90f743986a54fd6f7284b974f
marcosgabrielsr <marcosdevse@gmail.com>
2026-08-19 22:25:31 -0400

    feat: Adding new lstm_grid_search version

--- main_lstm.py
+++ main_lstm.py
@@ -1,3 +1,5 @@
+import json
+import torch
 from pathlib import Path
 from src.core.dataset import Dataset
 from src.search.lstm_grid_search import GridSearchLSTM
@@ -9,34 +11,37 @@ def main():
     # Configurando caminhos para salvar os arquivos
     results_path = Path('./results')
     results_path.mkdir(exist_ok=True)
-    Path('./results/models').mkdir(exist_ok=True)
 
     # Divide os dados em treino, validação e teste
     train, val, test = dataset.split(train=0.6, val=0.2)
-
-    # Parametros do gridsearch
-    param_grid = {
-        "windows_size": [10, 20, 30],
-        "hidden_size": [32, 64, 128],
-        "num_layers": [1, 2, 3, 4, 5],
-        "learning_rate": [0.0001],
-        "dropout": [0.0, 0.5],
-        "batch_size": [128, 512, 1024],
-        "epoch": [30, 40, 50]
-    }
 
     # Definindo device (prioridade para gpu caso haja)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(device)
 
-    grid_search_lstm(
-        train_normalized,
-        val_normalized,
-        scaler_y,
-        param_grid,
-        device,
-        results_path
-    )
+    # Parametros do gridsearch
+    param_grid = {
+        "windows_size": [10, 20, 30],
+        "hidden_size": [64, 128, 256],
+        "num_layers": [1, 2, 3, 4, 5],
+        "learning_rate": [0.001],
+        "dropout": [0.0, 0.5],
+        "batch_size": [128, 512, 1024],
+    }
+
+    # Criando objeto GridSearchLSTM
+    gsearch = GridSearchLSTM(train, val, param_grid, device)
+    best_metric, best_model, best_params = gsearch.run(results_path)
+
+    # Salvando resultados
+    dict_results = {"best_evm": best_metric}
+    dict_results.update(best_params)
+
+    with open(results_path/"best_result.json", "w") as f:
+        json.dump(dict_results, f)
+
+    # Salvando modelo
+    torch.save(best_model.state_dict(), results_path/"best_model.pkl")
 
 if __name__ == "__main__":
     main()
